98de5d59a6c61a7c8dee833df96cf6ea7ad4e80b
Tomer Admon <Tomer.a@qualisystems.com>
2022-05-30 21:40:08 -0500

    update code block to use dracula theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,7 +113,7 @@ const config = {
         ]
       },
       prism: {
-        theme: lightCodeTheme,
+        theme: require('prism-react-renderer/themes/dracula'),
         darkTheme: darkCodeTheme,
       },
     }),
