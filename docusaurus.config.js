6f9bc1076b57e0e2014640938f93253428a37347
Tomer Admon <Tomer.a@qualisystems.com>
2022-06-03 19:53:57 -0500

    fix edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,13 +30,13 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/QualiTorque/torque-docs/tree/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/QualiTorque/torque-docs/tree/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
