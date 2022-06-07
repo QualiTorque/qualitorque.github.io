c08111a922bc3e1ff35528a2585d51efff29ba79
Tomer Admon <Tomer.a@qualisystems.com>
2022-06-07 13:10:18 +0300

    update root folder

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Torque',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/torque-docs/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
