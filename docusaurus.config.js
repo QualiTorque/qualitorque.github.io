e59f18cc209e3e246e3d1a2e4faaf9fd0f4c9c6e
DvirYadaiQuali <91822368+DvirYadaiQuali@users.noreply.github.com>
2022-06-12 14:25:41 +0300

    moving all topbar css to scss file and changing right icons color

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,7 @@ const config = {
             require.resolve('./src/css/breadcrumbs.scss'),
             require.resolve('./src/css/footer.scss'),
             require.resolve('./src/css/dark-mode.scss'),
+            require.resolve('./src/css/topbar.scss'),
           ],
         },
       }),
