8c5a54c7bf6e66d94065e0c1a0e206901888deba
kalsky <yanivka@gmail.com>
2025-09-03 16:04:32 -0500

    have different config file for dev and prod

--- docusaurus-prod.config.js
+++ docusaurus-prod.config.js
@@ -20,22 +20,22 @@ const config = {
     v4: true,
     experimental_faster: true,
   },
-  // scripts: [
-  //   // One Trust Cookie policy
-  //   {
-  //     src: 'https://cdn.cookielaw.org/consent/31859194-4ed7-4958-bfc5-08046e0f6ab7/OtAutoBlock.js',
-  //     type: "text/javascript",
-  //   },
-  //   {
-  //     src: 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js',
-  //     charset: 'UTF-8',
-  //     type: "text/javascript",
-  //     "data-domain-script": "31859194-4ed7-4958-bfc5-08046e0f6ab7"
-  //   },
-  //   {  src: 'http://docs.qtorque.io/one-trust.js',
-  //     type: "text/javascript"
-  //   }
-  // ],
+  scripts: [
+    // One Trust Cookie policy
+    {
+      src: 'https://cdn.cookielaw.org/consent/31859194-4ed7-4958-bfc5-08046e0f6ab7/OtAutoBlock.js',
+      type: "text/javascript",
+    },
+    {
+      src: 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js',
+      charset: 'UTF-8',
+      type: "text/javascript",
+      "data-domain-script": "31859194-4ed7-4958-bfc5-08046e0f6ab7"
+    },
+    {  src: 'http://docs.qtorque.io/one-trust.js',
+      type: "text/javascript"
+    }
+  ],
   plugins: [
     require.resolve("docusaurus-plugin-sass")
   ],
