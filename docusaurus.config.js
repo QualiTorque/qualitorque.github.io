eb8777a255070b68f77a2d8c8d83ce5080fa7b6a
kalsky <yanivka@gmail.com>
2025-09-05 09:40:48 -0500

    remove sidebar file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,6 @@ const config = {
       ({
         docs: {
           routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/QualiTorque/torque-docs/tree/master/',
         },
